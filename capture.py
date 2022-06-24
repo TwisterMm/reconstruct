0a5c310c2d609aba7155577fd6e1e79d1524901e
WsTee <wesleytee@gmail.com>
2022-06-24 12:15:16 +0800

    fix close button

--- capture.py
+++ capture.py
@@ -99,7 +99,7 @@ if __name__ == '__main__':
 
             
                 
-            if key in (27, ord("q")) or cv2.getWindowProperty('RealSense', cv2.WND_PROP_AUTOSIZE) < 0: 
+            if key in (27, ord("q")) or cv2.getWindowProperty('RealSense', cv2.WND_PROP_VISIBLE) < 0: 
                 cv2.destroyAllWindows()
                 break
             if key == ord("s"):
