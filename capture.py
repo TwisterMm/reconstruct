d8c4eb0da2594cd78d6ba0883fa3b2e699e9d1de
WsTee <wesleytee@gmail.com>
2022-07-03 12:15:51 +0800

    close window fixed

--- capture.py
+++ capture.py
@@ -101,7 +101,7 @@ if __name__ == '__main__':
                 
 
 
-            if key in (27, ord("q")) or cv2.getWindowProperty('RealSense', cv2.WND_PROP_VISIBLE) < 0: 
+            if key in (27, ord("q")) or cv2.getWindowProperty('RealSense', cv2.WND_PROP_VISIBLE) < 1: 
                 cv2.destroyAllWindows()
                 break
             if key == ord("s"):
